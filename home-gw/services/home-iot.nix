1f6ef21888270e9d6c4704b6caf6dfdf39b4e318
Jakub Pravda <jacfal.tech@protonmail.com>
2022-07-21 17:40:26 +0200

    home iot conf changes

--- home-gw/services/home-iot.nix
+++ home-gw/services/home-iot.nix
@@ -8,7 +8,7 @@
       permit_join = true;
       mqtt = {
         base_topic = "zigbee2mqtt";
-        server = "mqtt://mqtt";
+        server = "mqtt://localhost:1883";
       };
       serial.port = "/dev/ttyUSB0";
       frontend.port = 8080;
