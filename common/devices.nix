eb2dce1852504ee9b7f78e5871153d363c7080cf
Jakub Pravda <jacfal.tech@protonmail.com>
2022-10-30 08:47:26 +0100

    new device

--- common/devices.nix
+++ common/devices.nix
@@ -6,13 +6,19 @@
       { id        = "0x00124b00251f6180";
         name      = "son-sns-01";
         type      = "SNZB-02";
-        location  = "livr";
+        location  = "livingroom";
       }
 
       { id        = "0x00124b0025132f8e";
         name      = "son-sns-02";
         type      = "SNZB-02";
-        location  = "bthrm";
+        location  = "bathroom";
+      }
+
+      { id        = "0x00124b002513284a";
+        name      = "son-sns-03";
+        type      = "SNZB-02";
+        location  = "bedroom";
       }
     ];
   };
