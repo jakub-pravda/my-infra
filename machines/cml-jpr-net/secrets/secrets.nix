f6f33dbcff47bc1b7f2d5f2802680fb229aa661a
Jakub Pravda <jacfal.tech@protonmail.com>
2023-06-22 21:06:45 +0200

    Invalid key

--- machines/cml-jpr-net/secrets/secrets.nix
+++ machines/cml-jpr-net/secrets/secrets.nix
@@ -1,5 +1,5 @@
 let
-  system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPzlaCdKDGbuPdfTtJuvswLCYKA2iQGa4xzS8wCb1yfM";
+  system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOxpj5PPnDiujOmld4K9hi1SA+PJVUaW1gN0SYw/9Oa/";
 in
 {
   "datadog.age".publicKeys = [ system ];
