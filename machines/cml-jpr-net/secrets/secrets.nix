ac513d0d5a7a7e0138b5ae2dc6a3d4d0d6b314e6
Jakub Pravda <jacfal.tech@protonmail.com>
2023-06-22 21:01:46 +0200

    Using system key for agenix - cml

--- machines/cml-jpr-net/secrets/secrets.nix
+++ machines/cml-jpr-net/secrets/secrets.nix
@@ -1,7 +1,6 @@
 let
-  jacob = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILzk+JhkeYhc/PWG30HIDMPhSnhxIE6H3qnhyu7ekP9T";
-  system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO2mLfva/VYFe71BRZBsMBuvDYIu7dGtL5EIA9x1bGv+";
+  system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPzlaCdKDGbuPdfTtJuvswLCYKA2iQGa4xzS8wCb1yfM";
 in
 {
-  "datadog.age".publicKeys = [ system jacob ];
+  "datadog.age".publicKeys = [ system ];
 }
