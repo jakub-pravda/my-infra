1e3554473c4c99b6bd3c90c4b5a3ff72dcf7d98b
Jakub Pravda <jacfal.tech@protonmail.com>
2023-10-12 22:12:13 +0200

    new tmp sensor

--- machines/home-gw/iot-hub.nix
+++ machines/home-gw/iot-hub.nix
@@ -1,19 +1,25 @@
-{
+let
+  livingRoomLocation = "livingroom";
+  bathroomLocation = "bathroom";
+  bedroomLocation = "bedroom";
+  hallLocation = "hall";
+  outsideLocation = "outside";
+in {
   id = "myhome-kr";
   devices = [
     # living room
     {
-      id = "0x00124b00251f6180";
-      name = "son-sns-01";
-      type = "SNZB-02";
-      location = "livingroom";
+      id = "0x00158d00094a0833";
+      name = "xia-tmp-02";
+      type = "WSDCGQ11LM";
+      location = livingRoomLocation;
     }
 
     {
       id = "0x50325ffffe5720d4";
       name = "danfoss-thermo-01";
       type = "014G2461";
-      location = "livingroom";
+      location = livingRoomLocation;
     }
 
     # bathroom
@@ -21,7 +27,7 @@
       id = "0x00124b0025132f8e";
       name = "son-sns-02";
       type = "SNZB-02";
-      location = "bathroom";
+      location = bathroomLocation;
     }
 
     # bedroom
@@ -29,28 +35,36 @@
       id = "0x00124b002513284a";
       name = "son-sns-03";
       type = "SNZB-02";
-      location = "bedroom";
+      location = bedroomLocation;
     }
 
     {
       id = "0xccccccfffe39fdca";
       name = "light-01";
       type = "IMM-LED";
-      location = "bedroom";
+      location = bedroomLocation;
     }
 
     {
       id = "0xccccccfffe3cd428";
       name = "light-02";
       type = "IMM-LED";
-      location = "bedroom";
+      location = bedroomLocation;
     }
 
     {
       id = "0xf082c0fffe99e1a0";
       name = "danfoss-thermo-02";
       type = "014G2461";
-      location = "bedroom";
+      location = bedroomLocation;
+    }
+
+    # hall
+    {
+      id = "0x00124b00251f6180";
+      name = "son-sns-01";
+      type = "SNZB-02";
+      location = hallLocation;
     }
 
     # outside
@@ -58,7 +72,7 @@
       id = "0x00158d00091eb8df";
       name = "xia-tmp-01";
       type = "WSDCGQ11LM";
-      location = "outside";
+      location = outsideLocation;
     }
   ];
 }
