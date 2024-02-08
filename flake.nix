d3e3a67e64dcd5971061db6e539200cce620ba41
Jakub Pravda <me@jakubpravda.net>
2024-02-08 21:51:06 +0100

    Update nix packages to 23.11 (I have a delay, I know...)

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My machines";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05-small";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11-small";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     agenix.url = "github:ryantm/agenix";
