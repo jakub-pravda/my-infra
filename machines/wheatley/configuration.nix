9625dd4a8a9caafb1190c00078c1c23df3f06ff1
Jakub Pravda <me@jakubpravda.net>
2024-05-05 17:38:08 +0200

    Wheatley config clean

--- machines/wheatley/configuration.nix
+++ machines/wheatley/configuration.nix
@@ -1,44 +1,29 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
 {
   config,
   pkgs,
   ...
 }: {
   imports = [
-    # Include the results of the hardware scan.
     ./hardware-configuration.nix
+    ./users.nix
   ];
 
-  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
   boot.initrd.luks.devices."luks-4e20ad04-f84e-4f9c-ac1d-ef95612eb7a6".device = "/dev/disk/by-uuid/4e20ad04-f84e-4f9c-ac1d-ef95612eb7a6";
+
   networking.hostName = "wheatley";
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  nix = {
-    trustedUsers = ["root" "jacob"];
-    extraOptions = ''
-      experimental-features = nix-command flakes
-    '';
-  };
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
   time.timeZone = "Europe/Prague";
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
+  nix.trustedUsers = ["root" "jacob"];
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
 
+  # Select internationalisation properties
+  i18n.defaultLocale = "en_US.UTF-8";
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "cs_CZ.UTF-8";
     LC_IDENTIFICATION = "cs_CZ.UTF-8";
@@ -51,10 +36,10 @@
     LC_TIME = "cs_CZ.UTF-8";
   };
 
-  # Enable the X11 windowing system.
+  # Enable the X11 windowing system
   services.xserver.enable = true;
 
-  # Enable the Gnome Desktop Environment.
+  # Enable the Gnome Desktop Environment
   services.xserver.displayManager.gdm = {
     enable = true;
     wayland = true;
@@ -67,10 +52,10 @@
     xkbVariant = "";
   };
 
-  # Enable CUPS to print documents.
+  # Enable CUPS to print documents
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
+  # Enable sound with pipewire
   sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
@@ -79,55 +64,18 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # Define a user account. Don't forget to set a password with ‘passwd’
   programs.zsh.enable = true;
   users.defaultUserShell = pkgs.zsh;
-  users.users.jacob = {
-    isNormalUser = true;
-    description = "Jacob";
-    extraGroups = ["networkmanager" "wheel"];
-  };
-
-  # Allow unfree packages
-  # nixpkgs.config.allowUnfree = true;
 
   # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
     gnome.gnome-tweaks
     libgcc
     vim
   ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
---
+++ machines/wheatley/users.nix
@@ -0,0 +1,10 @@
+{...}: {
+  users.users.jacob = {
+    isNormalUser = true;
+    home = "/home/jacob";
+    description = "Jacob";
+    extraGroups = ["wheel" "networkmanager"];
+  };
+
+  nix.settings.allowed-users = ["jacob"];
+}
