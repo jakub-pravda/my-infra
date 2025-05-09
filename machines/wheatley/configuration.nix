13c28b85917ebd2b7411f867786525d7e8c6596f
Jakub Pravda <me@jakubpravda.net>
2025-05-09 19:45:31 +0200

    Wheatley - add jupyter hub

--- machines/wheatley/configuration.nix
+++ machines/wheatley/configuration.nix
@@ -62,6 +62,39 @@
         variant = "";
       };
     };
+    
+    jupyterhub = {
+      enable = true;
+      host = "127.0.0.1";
+      port = 8000;
+      extraConfig = ''
+        # Use any/username password combination to login
+        c.JupyterHub.authenticator_class = "dummy"
+      '';
+      kernels = {
+        python3 = let
+          env = (pkgs.python3.withPackages (pythonPackages: with pythonPackages; [
+                  ipykernel
+                  matplotlib
+                  pandas
+                  seaborn
+                  scikit-learn
+                ]));
+        in {
+          displayName = "Python 3 for machine learning";
+          argv = [
+            "${env.interpreter}"
+            "-m"
+            "ipykernel_launcher"
+            "-f"
+            "{connection_file}"
+          ];
+          language = "python";
+          logo32 = "${env}/${env.sitePackages}/ipykernel/resources/logo-32x32.png";
+          logo64 = "${env}/${env.sitePackages}/ipykernel/resources/logo-64x64.png";
+        };
+      };
+    };
 
     pulseaudio.enable = false;
 
