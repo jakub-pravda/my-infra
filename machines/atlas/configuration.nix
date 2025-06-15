73468d0defa97e87cba5f03602964914600e5f91
Jakub Pravda <me@jakubpravda.net>
2025-06-15 22:11:25 +0200

    Add git

--- machines/atlas/configuration.nix
+++ machines/atlas/configuration.nix
@@ -77,6 +77,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    git
     #influxdb3
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
