6064761e6f22e7475210337a2c1d90d169ce6916
Jakub Pravda <me@jakubpravda.net>
2026-06-13 21:26:39 +0200

    nixos 26.05 bump

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My machines";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-my.url = "github:jakub-pravda/nixpkgs/shadow-pc";
 
